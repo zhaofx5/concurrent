39a0cc1a3a17f99fe12935ca1c041431b01dd938
zhaofx5 <zhaofx5@qq.com>
2018-04-03 22:05:12 +0800

    first commit

---
+++ src/com/zhaofx/learn/thread/TestMain.java
@@ -0,0 +1,28 @@
+/** 
+ * Project Name:Concurrent 
+ * File Name:TestMain.java 
+ * Package Name:com.zhaofx.learn.thread 
+ * Date:2018年4月3日下午9:45:02 
+ * Copyright (c) 2018, zhaofx5@qq.com All Rights Reserved. 
+ * 
+*/
+
+package com.zhaofx.learn.thread;
+
+/**
+ * ClassName:TestMain <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason: TODO ADD REASON. <br/>
+ * Date: 2018年4月3日 下午9:45:02 <br/>
+ * 
+ * @author zhaofengxiang
+ * @version
+ * @since JDK 1.8
+ * @see
+ */
+public class TestMain {
+	public static void main(String[] args) {
+		System.out.println("This is my github first file.");
+	}
+
+}
